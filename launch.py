85dea5e360d4eb34dd74689625abeca2de0b1984
Kevin <walchko@users.noreply.github.com>
2017-04-04 10:58:23 -0600

    Update launch.py

--- launch.py
+++ launch.py
@@ -3,10 +3,19 @@
 from __future__ import print_function
 from __future__ import division
 import pygecko
-import opencvutils as cvu
+# import opencvutils as cvu
+from opencvutils import MJPEGServer
+import Socket
+
+# need to run this in another process because of OpenCV and GIL
+def mjpeg(port=9000, win=(640,480)):
+  MJPEGServer.cam = MJPEGServer.setUpCamera(pi=True, win=win)
+	server = HTTPServer(('0.0.0.0', args['port']), MJPEGServer)
+	print "server started on {}:{}".format(Socket.gethostname(), port)
+	server.serve_forever()
 
 def run():
-  pass
-
+  mjpeg()
+  
 if __name__ == '__main__':
   run()
