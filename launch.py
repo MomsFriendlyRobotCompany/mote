fdb2dc8b54c5d7194639457444c32c20d5e2bfca
Kevin <walchko@users.noreply.github.com>
2017-04-04 10:45:06 -0600

    Create launch.py

---
+++ launch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from __future__ import division
+import pygecko
+import opencvutils as cvu
+
+def run():
+  pass
+
+if __name__ == '__main__':
+  run()
