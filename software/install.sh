46dd68a5f66cbc2fda2c736ec63b5924dd51bfab
Kevin <walchko@users.noreply.github.com>
2017-04-30 13:09:17 -0600

    Update install.sh

--- software/install.sh
+++ software/install.sh
@@ -27,18 +27,25 @@ apt-get -y install cmake pkg-config build-essential git libpcap0.8-dev
 
 # admin
 apt-get -y install nmap htop samba samba-common-bin arp-scan wget curl
-apt-get -y install figlet
 
 # linux kernel
 apt-get -y install raspi-config rpi-update
 
 # ascii art
-apt-get -y install jp2a
+apt-get -y install jp2a figlet
 
 # install node.js
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 apt-get install -y nodejs
 npm install -g httpserver
+
+# python 2/3
+# run as pi?: sudo su - pi -c "commands"
+su - pi -c "pip install -U pip wheel setuptools"
+pip install -U -r requirements.txt
+
+pip3 install -U pip wheel setuptools
+pip3 install -U -r requirements.txt
 
 echo ""
 echo "============================="
