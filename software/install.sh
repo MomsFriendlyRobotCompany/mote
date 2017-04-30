00b325db225de812993ecd06144c50f050eb52fa
Kevin <walchko@users.noreply.github.com>
2017-04-30 15:51:00 -0600

    updates

--- software/install.sh
+++ software/install.sh
@@ -31,8 +31,17 @@ apt-get -y install nmap htop samba samba-common-bin arp-scan wget curl
 # linux kernel
 apt-get -y install raspi-config rpi-update
 
+# bluetooth
+apt-get -y install bluez libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
+
 # ascii art
 apt-get -y install jp2a figlet
+
+# get updated packages
+# curl -s https://packagecloud.io/install/repositories/walchko/robots/script.deb.sh | sudo bash
+# apt-get update
+# apt-get -y install python3k
+dpkg -i python3k libopencv3 zeromq-kevin
 
 # install node.js
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
@@ -41,11 +50,18 @@ npm install -g httpserver
 
 # python 2/3
 # run as pi?: sudo su - pi -c "commands"
+cd tmp && wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py
 su - pi -c "pip install -U pip wheel setuptools"
 pip install -U -r requirements.txt
 
 pip3 install -U pip wheel setuptools
 pip3 install -U -r requirements.txt
+
+# update kernel
+rpi-update
+
+
+
 
 echo ""
 echo "============================="
