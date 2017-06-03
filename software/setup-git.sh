402d9a4f291fb528c9629829520551b43019ef48
Kevin <walchko@users.noreply.github.com>
2017-06-03 16:47:06 -0600

    Create setup-git.sh

---
+++ software/setup-git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+if [[ $# -eq 0 ]] ; then
+  echo "-------------------------------------"
+  echo "Please supply a github user name"
+  echo "-------------------------------------"
+  exit 1
+fi
+
+NAME=$1
+
+git config --global user.name ${NAME}
+git config --global user.email ${NAME}@users.noreply.github.com
+git config --global push.default simple
+
+echo ""
+echo "Done ..."
+echo ""
