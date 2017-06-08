fdab6e24ed411f3f1d2e0584306f694bc0159fda
walchko <walchko@users.noreply.github.com>
2017-06-08 03:13:21 +0000

    fix setup-ssh

--- software/setup-ssh.sh
+++ software/setup-ssh.sh
@@ -3,7 +3,7 @@
 set -e
 
 # check if we are root
-if [ "$EUID" -ne 0 ]
+if [ "$EUID" -eq 0 ]
 	then echo "Please run as root"
 	exit 1
 fi
@@ -12,11 +12,11 @@ USR="pi"
 HOME="/home/${USR}"
 
 if [ ! -f "${HOME}/.ssh/id_rsa" ]; then
-	su ${USR} -c "ssh-keygen -q -N \"\" -f ${HOME}/.ssh/id_rsa -t rsa"
+	ssh-keygen -q -N "" -f ${HOME}/.ssh/id_rsa -t rsa
 else
 	echo ""
 	echo "ssh already setup"
 fi
 
-su ${USR} -c "ssh-keygen -lvf ${HOME}/.ssh/id_rsa.pub"
+ssh-keygen -lvf ${HOME}/.ssh/id_rsa.pub
 echo ""
