c67f224af08d7c76d9503ef9d95b27bd77683cc1
Kevin <walchko@users.noreply.github.com>
2017-07-06 16:42:54 -0600

    Update install.sh

--- software/install.sh
+++ software/install.sh
@@ -25,7 +25,7 @@ echo "============================="
 echo ""
 
 # programming
-apt-get -y install cmake pkg-config build-essential git python-dev swig
+apt-get -y install cmake pkg-config build-essential git swig
 
 # admin
 apt-get -y install nmap htop samba samba-common-bin arp-scan wget curl libpcap0.8-dev
@@ -33,13 +33,14 @@ apt-get -y install nmap htop samba samba-common-bin arp-scan wget curl libpcap0.
 # linux kernel
 apt-get -y install raspi-config rpi-update
 
+# moved to install-python.sh
 # numpy
 # need atlas | blas | f2py | fortran
-apt-get -y install libatlas-base-dev gfortran
-
+# apt-get -y install libatlas-base-dev gfortran
+#
 # python3
-apt-get -y install libmpdec2
-apt-get -y install python3
+# apt-get -y install libmpdec2
+# apt-get -y install python3
 
 # bluetooth
 apt-get -y install bluez libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
