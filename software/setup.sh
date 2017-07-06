5e57aaf7e503505fd2371ebc068e7b55e402374d
Kevin <walchko@users.noreply.github.com>
2017-07-06 16:21:36 -0600

    Update setup.sh

--- software/setup.sh
+++ software/setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
+USEAGE="setup.sh HOSTNAME WIFI-SSID WIFI-PASSWORD
+
 # check if we are root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+if [ "$EUID" -ne 0 ] ; then
+  echo "Please run as root"
   exit 1
 fi
 
-if [[ $# -eq 0 ]] ; then
+if [[ $# -ne 3 ]] ; then
   echo "Please supply a hostname"
+  echo $USEAGE
   exit 1
 fi
 
@@ -37,14 +40,17 @@ raspi-config nonint do_memory_split $GPUMEMORY
 
 # create temp and git folder
 su - pi -c "mkdir -p ~/tmp"
-su - pi -c "mkdir -p ~/github"
-su - pi -c "mkdir -p ~/bitbucket"
+# su - pi -c "mkdir -p ~/github"  # you are probably already running from here :P
+# su - pi -c "mkdir -p ~/bitbucket"
 
 # # samba setup
 ./setup-smb.sh
 
 # commandline setup
 ./setup-dotfiles.sh
+
+# setup wifi
+./setup-wifi.sh $2 $3
 
 # just in case root changed a permission in ~
 chown -R pi:pi /home/pi
