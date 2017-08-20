e6562ea215bdf114ec4d6444b18f4a953d118ecd
Kevin <walchko@users.noreply.github.com>
2017-08-20 12:44:48 -0600

    Update upgrade-os.sh

--- software/upgrade-os.sh
+++ software/upgrade-os.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # https://linuxconfig.org/raspbian-gnu-linux-upgrade-from-jessie-to-raspbian-stretch-9
 #
+# This isn't perfect ... there are places where you have to answer questions
+# certificates - q (quit ... stupid notice)
+# bashrc - n (no - keep your bashrc file)
+# login.def - y (yes - upgrade it)
+# gpg - q (quit ... stupid notice)
+# services - y (yes - upgrade services w/o asking me on each one)
 
 set -e
 
@@ -35,7 +41,7 @@ apt-get upgrade -y
 apt-get dist-upgrade -y
 apt-get autoremove -y
 
-# there are stupid dialogs that open up ... try to stop them
+# there are stupid dialogs that open up ... try to stop them (don't know if this will work)
 export DEBIAN_FRONTEND=noninteractive
 
 # change distro name in repo lists
@@ -48,6 +54,7 @@ sed -i "s/${OLD_DISTRO}/${DISTRO}/g" /etc/apt/sources.list.d/raspi.list
 apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
+apt-get autoremove  # there is some old stuff now ... get rid of it
 
 # get back git
 apt-get install git -y
