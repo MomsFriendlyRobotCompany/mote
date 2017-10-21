29577d528cd3e9a07f1753369d7b2adf2f42982a
walchko <walchko@users.noreply.github.com>
2017-10-21 12:13:34 -0600

    fix autoupgrade

--- software/setup-auto-upgrade.sh
+++ software/setup-auto-upgrade.sh
@@ -13,25 +13,31 @@ echo "| Setting Up Auto Upgrade   |"
 echo "============================="
 echo ""
 
+echo "*** setup script ***"
+
 # setup the service
-AUTOUPGRADE_FILE="/etc/cron.weekly/autoupgrade.sh"
+REREAD=""
+SCRIPT="static/autoupgrade.sh"
+TIMER="/etc/systemd/system/autoupgrade.timer"
+SERVICE="/etc/systemd/system/autoupgrade.service"
+
 
 # if the file exists, remove it ... going to dynamically create it
-if [[ -f "${AUTOUPGRADE_FILE}" ]]; then
-	rm ${AUTOUPGRADE_FILE}
+if [[ -f "${TIMER}" ]]; then
+  REREAD="true"
 fi
 
-cat <<'EOF' >${AUTOUPGRADE_FILE}
+cat <<'EOF' >${SCRIPT}
 #!/bin/bash
 
 # check online
 wget -q --spider http://google.com
 
 if [ $? -eq 0 ]; then
-    echo "*** Online ***"
+  echo "*** Online ***"
 else
-    echo "*** Offline, no access to internet ***"
-    return 1
+  echo "*** Offline, no access to internet ***"
+  return 1
 fi
 
 echo "*** upgrade base system ***"
@@ -40,10 +46,10 @@ apt-get upgrade -y
 apt-get autoclean
 
 echo "*** upgrade python 2 ***"
-pip list --outdated | cut -d' ' -f1 | xargs pip install --upgrade
+pip list --format=legacy --outdated | cut -d' ' -f1 | xargs pip install --upgrade
 
 echo "*** upgrade python 3 ***"
-pip3 list --outdated | cut -d' ' -f1 | xargs pip3 install --upgrade
+pip3 list --format=legacy --outdated | cut -d' ' -f1 | xargs pip3 install --upgrade
 
 echo "*** update npm packages ***"
 npm i -g npm
@@ -52,7 +58,7 @@ npm update -g
 # remove the old file, so we don't get a million of them
 UFILE=`find /home/pi -name 'system-updated-on*'`
 if [[ ! -z "${UFILE}" ]]; then
-	rm ${UFILE}
+  rm ${UFILE}
 fi
 
 # let people know we upgraded things
@@ -66,4 +72,44 @@ exit 0
 EOF
 
 # fix permissions
-chmod 755 ${AUTOUPGRADE_FILE}
+chmod 755 ${SCRIPT}
+
+echo "*** setup timer ***"
+
+cat<<'EOF' > ${TIMER}
+[Unit]
+Description=Weekly update
+
+[Timer]
+OnCalendar=weekly
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+
+echo "*** setup service ***"
+
+cat <<EOF > ${SERVICE}
+[Unit]
+Description=Updates system weekly
+Wants=audoupgrade.timer
+
+[Service]
+Type=simple
+ExecStart=/home/pi/github/mote/software/static/autoupgrade.sh
+EOF
+
+if [[ -z "${REREAD}" ]]; then
+  echo "*** enabling/starting timer and service ***"
+  systemctl start autoupdate.timer
+  systemctl enable autoupdate.timer
+  systemctl start autoupdate.service
+  echo " to see timers, run: sudo systemctl list-timers --all"
+  echo " to see output, run: sudo journalctl -u autoupgrade"
+else
+  echo "*** need to reload service due to changes ***"
+  systemctl daemon-reload
+  systemctl start autoupdate.timer
+  systemctl start autoupdate.service
+fi
