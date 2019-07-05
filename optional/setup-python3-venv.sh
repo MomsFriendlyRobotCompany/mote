7b46b974161ce81ead2dd72eea75692ac92b2280
Kevin <walchko@users.noreply.github.com>
2019-07-05 15:52:17 -0400

    Update setup-python3-venv.sh

--- optional/setup-python3-venv.sh
+++ optional/setup-python3-venv.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-
+# Setup a virtual python3 env
+# sudo: yes
 set -e
 
 # check if we are root
-if [ "$EUID" -eq 0 ]
-  then echo "Please run as USER"
+if ! [ $(id -u) = 0 ]; then 
+  echo "Please run as ROOT"
   exit 1
 fi
 
-sudo apt-get install python3-venv python3-dev -y
+#--------------------------------------------------
+apt install -y python3-venv 
+apt install -y python3-dev
+#--------------------------------------------------
 
+su - pi <<'EOF'
 python3 -m venv /home/pi/.venv
-
 source /home/pi/.venv/bin/activate
-
 pip install -U pip setuptools wheel
-pip install numpy requests nose msgpack pyzmq twine
+pip install -U build_utils
+EOF
